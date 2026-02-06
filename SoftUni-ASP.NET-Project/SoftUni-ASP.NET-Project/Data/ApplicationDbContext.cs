44eec34cb5d72aac52230c15fd361dfdde465de4
Danail Georgiev <105545729+SkySideIT@users.noreply.github.com>
2026-02-06 18:34:36 +0200

    Application created

---
+++ SoftUni-ASP.NET-Project/SoftUni-ASP.NET-Project/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace SoftUni_ASP.NET_Project.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ SoftUni-ASP.NET-Project/SoftUni-ASP.NET-Project/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SoftUni_ASP.NET_Project.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
